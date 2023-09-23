810aef8e0d43d937ceaa66be4e004e65619d6e73
Susana Solis Flores <Suzy.flores@icloud.com>
2023-09-22 22:32:04 -0400

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const drivers = [
+{
+name: "Bobby", 
+},
+{
+name: "Sammy", 
+},
+{
+name: "Sally", 
+},
+{
+name: "Annette", 
+},
+{
+name: "Sarah", 
+},
+{
+name: "bobby"
+}]
+
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+  }
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) => driver.startsWith(letters));
+}
+function matchName(drivers, name) {
+return drivers.filter((driver) => driver.name === name);
+}
+
